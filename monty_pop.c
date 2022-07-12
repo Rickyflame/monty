e2db2679e7494decd9ac7f004f66de5234868674
Rickyflame <kimumuricky@gmail.com>
2022-07-12 12:29:31 +0300

    Implement the pop opcode

---
+++ monty_pop.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * m_pop - pop the top element from the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: line number of current operation
+ * Return: void has no return value
+ */
+void m_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *pop = *stack;
+
+	if (var.stack_len == 0)
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: can't pop an empty stack\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->prev = (*stack)->prev;
+	(*stack)->prev->next = (*stack)->next;
+	if (var.stack_len != 1)
+		*stack = (*stack)->next;
+	else
+		*stack = NULL;
+	free(pop);
+	var.stack_len--;
+}
