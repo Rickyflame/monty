77baac63fc34075a1010eedeebb829f57e7672c5
Rickyflame <kimumuricky@gmail.com>
2022-07-12 13:43:25 +0300

    Implement the nop opcode

---
+++ monty_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * m_nop - no operations performed
+ * @stack: pointer to head of the stack
+ * @line_number: number of current operation
+ * Return: void has no return value
+ */
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
