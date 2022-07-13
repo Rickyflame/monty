49cd35a5116331a50ae99ce67186082c0767ff14
Rickyflame <kimumuricky@gmail.com>
2022-07-13 12:12:53 +0300

    Implement the pchar opcode

---
+++ monty_pchar.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+#include <ctypes.h>
+
+/**
+ * m_pchar - print character from top of stack
+ * @stack: pointer to top of stack
+ * @line_number: number of current operation
+ * Return: void has no return value
+ */
+void m_pchar(stack_t **stack, unsigned int line_number)
+{
+	int ch;
+
+	if (var.stack_len < 1)
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: can't pchar, stack empty\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	ch = (*stack)->n;
+	if (!isascii(ch))
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: can't pchar, value out of range\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", ch);
+}
