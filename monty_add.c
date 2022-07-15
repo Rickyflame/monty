de44e69151ff2ab62ab0877d084e79790a50991e
Rickyflame <kimumuricky@gmail.com>
2022-07-15 09:48:47 +0300

    Implement the add opcode

--- monty_add.c
+++ monty_add.c
@@ -1,24 +1,33 @@
 #include "monty.h"
 
 /**
- * m_add - add the top two elements of the stack
- * @stack: double pointer tot he beginning of the stack
- * @line_number: script line number
- *
- * Return: void
- */
-void m_add(stack_t **stack, unsigned int line_number)
+ * f_add - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_add(stack_t **head, unsigned int counter)
 {
-	int n = 0;
+	stack_t *h;
+	int len = 0, aux;
 
-	if (var.stack_len < 2)
+	h = *head;
+	while (h)
 	{
-		dprintf(STDOUT_FILENO,
-			"L%u: can't add, stack too short\n",
-			line_number);
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	n += (*stack)->n;
-	m_pop(stack, line_number);
-	(*stack)->n += n;
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
 }
